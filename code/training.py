752a6d93dfe39cb37073e9377859962697730b88
DaniSode <danisode@student.chalmers.se>
2024-01-09 16:22:57 +0100

    Update training.py

--- code/training.py
+++ code/training.py
@@ -292,7 +292,7 @@ def train():
             question = sample['question'].to(device=device)
             label = sample['answer'].to(device=device)
             logits = model(image, question)
-            print(tf.shape(logits))
+            print(np.shape(logits))
             print(logits)
             loss = criterion(logits, label)
             epoch_loss['train'] += loss.item()
