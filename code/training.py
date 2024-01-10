648f337c48b877deb5d2c1d7e356149f768b582b
DaniSode <danisode@student.chalmers.se>
2024-01-10 13:51:48 +0100

    Update training.py

--- code/training.py
+++ code/training.py
@@ -161,7 +161,7 @@ class Vocab:
 
     def load_vocab(self, vocab_file):
         with open(vocab_file) as f:
-            vocab = [v.strip(), print(idx) for idx, v in enumerate(f)]
+            vocab = [v.strip() print(idx) for idx, v in enumerate(f)]
         return vocab
 
     def word2idx(self, vocab):
